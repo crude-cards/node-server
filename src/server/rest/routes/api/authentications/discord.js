c27559a468a8ad95aa5e2841ab5305dfc8439b51
Amish Shah <amishshah.2k@gmail.com>
2017-06-18 23:04:36 +0100

    Add POST /authentications/discord

---
+++ src/server/rest/routes/api/authentications/discord.js
@@ -0,0 +1,44 @@
+const Route = require('../../Route');
+const Discord = require('../../DiscordOAuth');
+
+class AuthenticateDiscord extends Route {
+	constructor(server) {
+		super('/api/authentications/discord', server);
+		this.discord = new Discord(this.server);
+	}
+
+	async post(req, res) {
+		const userID = this.ensureAuthorized(req);
+
+		if (!req.params.code) {
+			throw this.error(400, `Parameter 'code' must be supplied.`);
+		}
+
+		// Fetch discord details from OAuth code
+		try {
+			var discordID = await this.discord
+				.token(req.params.code)
+				.then(tokenData => this.discord.details(tokenData.access_token))
+				.then(data => data.id);
+		} catch (error) {
+			if (error.status === 401) {
+				throw this.error(401, `Invalid 'code' parameter supplied`);
+			}
+			throw error;
+		}
+
+		try {
+			var user = await this.data.db.one('UPDATE users SET discord_id=$1 WHERE id=$2 RETURNING *', [discordID, userID]);
+		} catch (error) {
+			if (error.code === '23505') {
+				throw this.error(400, 'Discord account is already linked.');
+			}
+			this.server.logger.error(error);
+			throw this.error(500, 'Error querying database.');
+		}
+
+		res.send({ user });
+	}
+}
+
+module.exports = AuthenticateDiscord;
